9cb25315e4746797f1904c34a980f7d17a753467
am-miracle <judemiraco0@gmail.com>
2024-07-24 02:04:46 +0100

    fixed combobox component error, separate type alias and update category-form.tsx

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,6 +9,7 @@ import {
   CommandGroup,
   CommandInput,
   CommandItem,
+  CommandList,
 } from "@/components/ui/command";
 import {
   Popover,
@@ -46,28 +45,30 @@ const Combobox = React.forwardRef<HTMLButtonElement, ComboboxProp>(
         </PopoverTrigger>
         <PopoverContent className="w-full p-0">
           <Command>
-            <CommandInput placeholder="Search option..." />
-            <CommandEmpty>No option found.</CommandEmpty>
-            <CommandGroup>
-              {options?.map((option) => (
-                <CommandItem
-                  key={option?.value}
-                  value={option?.value}
-                  onSelect={() => {
-                    onChange(option?.value === value ? "" : option?.value);
-                    setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === option?.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {option?.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
+            <CommandList>
+              <CommandInput placeholder="Search option..." />
+              <CommandEmpty>No option found.</CommandEmpty>
+              <CommandGroup>
+                {options?.map((option) => (
+                  <CommandItem
+                    key={option?.value}
+                    value={option?.value}
+                    onSelect={() => {
+                      onChange(option?.value === value ? "" : option?.value);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === option?.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {option?.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
           </Command>
         </PopoverContent>
       </Popover>
--- types/course.ts
+++ types/course.ts
@@ -1,25 +1,34 @@
-import { Course } from "@prisma/client";
-
+import { Attachment, Course } from "@prisma/client";
 
 export type TitleFormProps = {
   initialData: {
-      title: string;
+    title: string;
   };
   courseId: string;
-}
+};
 
 export type ImageFormProps = {
-  initialData: Course
+  initialData: Course;
   courseId: string;
 };
 
 export type DescriptionFormProps = {
   initialData: Course;
-  courseId: string
-}
+  courseId: string;
+};
 
 export type CategoryFormProps = {
-    initialData: Course;
-    courseId: string;
-    options: { label: string; value: string; }[];
-}; 
+  initialData: Course;
+  courseId: string;
+  options: { label: string; value: string }[];
+};
+
+export type PriceFormProps = {
+  initialData: Course;
+  courseId: string;
+};
+
+export type AttachmentFormProps = {
+  initialData: Course & { attachments: Attachment[] };
+  courseId: string;
+};
